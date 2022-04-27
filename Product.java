44a9b2865bcea29be3d331c816912d8391dc78fe
Nakul <nakuljoshi.1606@gmail.com>
2022-04-27 10:46:36 +0530

    Program to multiply two nos

---
+++ Product.java
@@ -0,0 +1,9 @@
+public class Product
+{  
+   public static void main(String args[])   
+ {  
+	int n1=Integer.parseInt(args[0]), n2=Integer.parseInt(args[1]), product; 
+	System.out.println(n1 +" "+ n2); 
+	product = n1 * n2;  
+	System.out.println("The sum of numbers is: "+product);  
+ }  
